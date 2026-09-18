0ab06406541a53cf7a4cfaebe726e0cfa0da5ce1
eaa24fnie <eaa24fnie@students.eaaa.dk>
2026-09-18 11:02:32 +0200

    Nu virker card

--- deck.js
+++ deck.js
@@ -1,18 +1,29 @@
+let deckID;
 
 async function getData() {
-  const url = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
+  const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-      const result = await response.json();
+    const result = await response.json();
     console.log(result);
-
-    } catch (error) {
+    console.log(result.deck_id);
+    deckID = result.deck_id;
+  } catch (error) {
     console.error(error.message);
   }
-};
+}
 
-document.getElementById("start").addEventListener("click", getData())
+getData();
 
+async function drawCard() {
+  const card = await fetch(
+    `https://deckofcardsapi.com/api/deck/${deckID}/draw/`,
+  );
+  const result = await card.json();
+  console.log(result);
+}
+
+document.getElementById("start").addEventListener("click", drawCard);
