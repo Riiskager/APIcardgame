3be11634b02e749dc55c4b9d48410ecbb22003bb
eaa24fnie <eaa24fnie@students.eaaa.dk>
2026-09-22 10:52:51 +0200

    Then virker

--- deck2.js
+++ deck2.js
@@ -1,31 +1,28 @@
-const deckID = "864qod44nqiy"
+const deckID = "864qod44nqiy";
 
 function shuffleDeck() {
-    return fetch("https://deckofcardsapi.com/api/deck/864qod44nqiy/shuffle/?deck_count=1")
-
-    .then(response => {
-        if (!response.ok) throw new Error('Failed');
-    return response.json();
-    
-})
-// .then(deck =>{
-//     console.log(deck)
-//     return fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/shuffle/?deck_count=1`)
-// })
-.catch(error =>{
-    console.log('Error:', error)
-} )
+  return fetch(
+    "https://deckofcardsapi.com/api/deck/864qod44nqiy/shuffle/?deck_count=1",
+  )
+    .then((response) => {
+      if (!response.ok) throw new Error("Failed");
+      return response.json();
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
 }
-function getCard(){
-return fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
-
-  .then(response => {
-        if (!response.ok) throw new Error('Failed');
-    return response.json();
-
-})
-
+function getCard() {
+  return fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
+    .then((response) => {
+      if (!response.ok) throw new Error("Failed");
+      return response.json();
+    })
+    .then((data) => {
+      const cardImage = data.cards[0].image;
+      document.getElementById("trukketkort").src = cardImage;
+    });
 }
 
-
+document.getElementById("start").addEventListener("click", getCard);
 shuffleDeck();
